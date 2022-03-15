d53f03cff8b07a904d6aee1950e192f1bf14bc81
Antón Vega <anton.paidoslalin@gmail.com>
2022-03-15 13:55:51 +0100

    20220315 morning session end

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-const jokeurl ='https://api.chucknorris.io/jokes/random';
+const jokeUrl ='https://api.chucknorris.io/jokes/random';
 
-fetch(jokeurl).then(resp => {
-    resp.json().then (data=>{
-        console.log(data.value);
-    })
-    
-})
+// fetch(jokeurl).then(resp => {
+//     resp.json().then (data=>{
+//         console.log(data.value);
+//     })
+//})
+
+fetch(jokeUrl)
+    .then(resp => resp.json())
+    .then(data=>console.log(data.value));
