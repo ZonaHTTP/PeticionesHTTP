547d8265b613aa87a9dd0d86b362ede51f3e7df0
Antón Vega <anton.paidoslalin@gmail.com>
2022-03-18 13:19:36 +0100

    Engadir http-provider.js

---
+++ src/js/http-provider.js
@@ -0,0 +1,20 @@
+const jokeUrl='https://api.chucknorris.io/jokes/random';
+
+const obtenerChiste=async() =>{
+
+    try {
+        const resp=await fetch(jokeUrl);
+
+        if (!resp.ok) throw 'No se pudo realizar la peticion';
+
+        return await resp.json();
+    } catch (error) {
+        throw error;
+    }
+
+}
+
+
+export{
+    obtenerChiste
+}
