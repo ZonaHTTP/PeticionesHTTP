63462d17cdd1a874845d7a08dcbf6aca9654b1bb
Forza Catro <forza4@gmail.com>
2022-03-31 13:15:17 +0200

    final

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 // import { init } from './js/usuarios-page';
 // import { obtenerChiste } from "./src/js/http-provider.js";
-import * as CRUD from './js/crud-provider';
+import {init} from './js/archivos-page';
 // obtenerChiste().then(console.log);
 // init();
-CRUD.getUsuario( 2 ).then (console.log);
+// CRUD.getUsuario( 2 ).then (console.log);
 
-CRUD.crearUsuario({
-    name:"Antón",
-    job:"Programador e deseñador de blogs"
-});
-CRUD.actualizarUsuario(1,{
-    name:"Antón Vega"
-});
-CRUD.borrarUsuario(1).then(console.log);
+// CRUD.crearUsuario({
+//     name:"Antón",
+//     job:"Programador e deseñador de blogs"
+// });
+// CRUD.actualizarUsuario(1,{
+//     name:"Antón Vega"
+// });
+// CRUD.borrarUsuario(1).then(console.log);
